8dee1fb3bab074f5790ad94e033f1a3b9c613cea
ashishcoderoofs <141501346+coderoofsit@users.noreply.github.com>
2025-08-14 06:42:11 -0400

    fix OTP 111111

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1421,5 +1421,5 @@ function generateRandomOTP() {
   // Generate a random 6-digit OTP
   const min = 100000;
   const max = 999999;
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return 111111;
 }
